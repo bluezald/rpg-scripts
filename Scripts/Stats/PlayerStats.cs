0ac9c0b51817c98561149eea514e6c99050361c7
Vincent Bacalso <vincent.bacalso@trustarc.com>
2018-10-27 22:31:12 +0800

    - added stats scripts

---
+++ Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats {
+
+	void Start () 
+	{
+		EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;	
+	}
+
+	void OnEquipmentChanged (Equipment newItem, Equipment oldItem) 
+	{
+		if (newItem != null) {
+			armor.AddModifier (newItem.armorModifier);
+			damage.AddModifier (newItem.damageModifier);
+		}
+
+		if (oldItem != null) {
+			armor.RemoveModifier (newItem.armorModifier);
+			damage.RemoveModifier (newItem.damageModifier);
+		}
+	}
+}
