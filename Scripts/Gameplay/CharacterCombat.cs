82a05a2de3cc8c84badc4fcb10155bcec3cda3a1
Vincent Bacalso <vincent.bacalso@trustarc.com>
2018-11-03 15:28:55 +0800

    - add handling on animation change when attacking - add handling on animation when there's an equipment change

--- Scripts/Gameplay/CharacterCombat.cs
+++ Scripts/Gameplay/CharacterCombat.cs
@@ -7,9 +7,12 @@ public class CharacterCombat : MonoBehaviour {
 
 	public float attackSpeed = 1f;
 	private float attackCooldown = 0f;
+	const float combatCooldown = 5;
+	float lastAttackTime;
 
 	public float attackDelay = 0.6f;
 
+	public bool inCombat { get; private set; }
 	public event System.Action OnAttack;
 
 	CharacterStats myStats;
@@ -20,6 +23,10 @@ public class CharacterCombat : MonoBehaviour {
 
 	void Update() {
 		attackCooldown -= Time.deltaTime;
+
+		if (Time.time - lastAttackTime > combatCooldown) {
+			inCombat = false;
+		}
 	}
 
 	public void Attack(CharacterStats targetStats) {
@@ -31,6 +38,8 @@ public class CharacterCombat : MonoBehaviour {
 				OnAttack ();
 
 			attackCooldown = 1f / attackSpeed;
+			inCombat = true;
+			lastAttackTime = Time.time;
 		}
 	}
 
@@ -38,5 +47,9 @@ public class CharacterCombat : MonoBehaviour {
 		yield return new WaitForSeconds (delay);
 
 		stats.TakeDamage (myStats.damage.GetValue ());
+
+		if (stats.currentHealth <= 0) {
+			inCombat = false;
+		}
 	}
 }
--- Scripts/Player/CharacterAnimator.cs
+++ Scripts/Player/CharacterAnimator.cs
@@ -5,22 +5,41 @@ using UnityEngine.AI;
 
 public class CharacterAnimator : MonoBehaviour {
 
+	public AnimationClip replaceableAttackAnimation;
+	public AnimationClip[] defaultAttackAnimationSet;
+	protected AnimationClip[] currentAttackAnimationSet;
+
 	const float locomotionAnimationSmoothTime = .1f;
 
 	NavMeshAgent agent;
-	Animator animator;
-
+	protected Animator animator;
+	protected CharacterCombat combat;
+	protected AnimatorOverrideController overrideController;
 
 	// Use this for initialization
-	void Start () {
+	protected virtual void Start () {
 		agent = GetComponent<NavMeshAgent> ();
 		animator = GetComponentInChildren<Animator> ();
+		combat = GetComponent<CharacterCombat> ();
 
+		overrideController = new AnimatorOverrideController (animator.runtimeAnimatorController);
+		animator.runtimeAnimatorController = overrideController;
+
+		currentAttackAnimationSet = defaultAttackAnimationSet;
+		combat.OnAttack += OnAttack;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	protected virtual void Update () {
 		float speedPercent = agent.velocity.magnitude / agent.speed;
 		animator.SetFloat ("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+
+		animator.SetBool ("inCombat", combat.inCombat);
+	}
+
+	protected virtual void OnAttack() {
+		animator.SetTrigger ("attack");
+		int attackIndex = Random.Range (0, currentAttackAnimationSet.Length);
+		overrideController [replaceableAttackAnimation.name] = currentAttackAnimationSet [attackIndex];
 	}
 }
