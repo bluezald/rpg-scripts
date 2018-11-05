c4f613e817225016d5ce77de21af3d786537b3d0
Vincent Bacalso <vincent.bacalso@trustarc.com>
2018-11-05 11:23:38 +0800

    - updated directory - added health ui bar script

---
+++ Scripts/UI/HealthUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour {
+
+	public GameObject uiPrefab;
+	public Transform target;
+
+	Transform uiTransform;
+	Image healthSlider;
+	Transform cameraTransform;
+
+	void Start() {
+		cameraTransform = Camera.main.transform;
+
+		foreach (Canvas c in FindObjectsOfType<Canvas>()) {
+			if (c.renderMode == RenderMode.WorldSpace) {
+				uiTransform = Instantiate (uiPrefab, c.transform).transform;
+				healthSlider = uiTransform.GetChild (0).GetComponent<Image> ();
+				break;
+			}
+		}
+	}
+
+	void LateUpdate() {
+		uiTransform.position = target.position;
+		uiTransform.forward = -cameraTransform.forward;
+	}
+
+
+}
