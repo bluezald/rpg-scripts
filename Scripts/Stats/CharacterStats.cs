4d2c8e442c99b9f0eefab178d3c1bd18fad93b2e
Vincent Bacalso <vincent.bacalso@trustarc.com>
2018-11-06 10:30:02 +0800

    - added event to update health ui

--- Scripts/Stats/CharacterStats.cs
+++ Scripts/Stats/CharacterStats.cs
@@ -8,6 +8,8 @@ public class CharacterStats : MonoBehaviour {
 	public Stat damage;
 	public Stat armor;
 
+	public event System.Action<int, int> OnHealthChanged;
+
 	void Awake() 
 	{
 		currentHealth = maxHealth;
@@ -27,6 +29,10 @@ public class CharacterStats : MonoBehaviour {
 
 		currentHealth -= damage;
 		Debug.Log (transform.name + " takes " + damage + " damage");
+
+		if (OnHealthChanged != null) {
+			OnHealthChanged (maxHealth, currentHealth);
+		}
 
 		if (currentHealth <= 0) {
 			Die ();
--- Scripts/UI/HealthUI.cs
+++ Scripts/UI/HealthUI.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(CharacterStats))]
 public class HealthUI : MonoBehaviour {
 
 	public GameObject uiPrefab;
 	public Transform target;
+	float visibleTime = 5;
 
+	float lastMadeVisibleTime;
 	Transform uiTransform;
 	Image healthSlider;
 	Transform cameraTransform;
@@ -19,14 +22,36 @@ public class HealthUI : MonoBehaviour {
 			if (c.renderMode == RenderMode.WorldSpace) {
 				uiTransform = Instantiate (uiPrefab, c.transform).transform;
 				healthSlider = uiTransform.GetChild (0).GetComponent<Image> ();
+				uiTransform.gameObject.SetActive (false);
 				break;
+			}
+		}
+
+		GetComponent<CharacterStats> ().OnHealthChanged += OnHealthChanged;
+	}
+
+	void OnHealthChanged(int maxHealth, int currentHealth) {
+		if (uiTransform != null) {
+			uiTransform.gameObject.SetActive (true);
+			lastMadeVisibleTime = Time.time;
+
+			healthSlider.fillAmount = (float)currentHealth / maxHealth;
+
+			if (currentHealth <= 0) {
+				Destroy (uiTransform.gameObject);
 			}
 		}
 	}
 
 	void LateUpdate() {
-		uiTransform.position = target.position;
-		uiTransform.forward = -cameraTransform.forward;
+		if (uiTransform != null) {
+			uiTransform.position = target.position;
+			uiTransform.forward = -cameraTransform.forward;
+
+			if (Time.time - lastMadeVisibleTime > visibleTime) {
+				uiTransform.gameObject.SetActive (false);
+			}
+		}
 	}
 
 
